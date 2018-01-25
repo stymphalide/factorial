145238724562b966d8279998e92a83dd26e263e5
stymphalide <angelo.birrer@gmail.com>
2018-01-25 12:06:03 +0100

    Added two faster functions.

--- factorial.ex
+++ factorial.ex
@@ -5,4 +5,11 @@ defmodule Factorial do
   def factorial(n) do 
     n * factorial(n-1)
   end
+
+  def factorial_tail(0, res) do
+    res
+  end
+  def factorial_tail(n, res) do
+    factorial_tail(n-1, n*res)
+  end
 end
--- factorial.py
+++ factorial.py
@@ -3,3 +3,14 @@ def factorial(n):
         return 1
     else:
         return n * factorial(n-1)
+
+def factorial_for(n):
+    if n == 0:
+        return 1
+    else:
+        res = 1
+        for e in range(1, n+1):
+            res = res * e
+        return res
+
+        
